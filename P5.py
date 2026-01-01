0f763768e7664c47c9c84426f9b067524ed1cdbd
prabhat01111999 <67739000+prabhat01111999@users.noreply.github.com>
2026-01-01 10:49:08 +0530

    Update P5.py

--- P5.py
+++ P5.py
@@ -5,6 +5,22 @@ def fib(n):
         a , b = b , a+b
     return a
 
+t = fib(20)
+print(t)
+
 def calc_mean(first, *reminder):
     mean = (first + sum(reminder))/(1+len(reminder))
     return mean
+
+
+#Recursion
+
+def fib_2(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1 
+    return fib_2(n-1) + fib(n-2)
+
+x = fib_2(20)
+print(x)
